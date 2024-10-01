6bc037006ef79d427b307c8325bd65c7816df894
María-José Mejía <165747579+MajitoBrinkitos@users.noreply.github.com>
2024-10-01 14:48:16 -0700

    ex03.js
    Missing Number

--- ex03.js
+++ ex03.js
@@ -2,4 +2,17 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
-console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
+//console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
+
+const arr = [1, 2, 4, 5, 6];
+
+function findMissingNumber(){
+
+    for(let i = 1; i <= arr.length + 1; i++){
+     if(!arr.includes(i)){
+        console.log(`${i} is the missing number`);
+        return i;
+         }
+    }
+}
+findMissingNumber();
