8ba64f912a4b5dd2bf852c66143eadf8652f796c
María-José Mejía <165747579+MajitoBrinkitos@users.noreply.github.com>
2024-10-01 10:44:48 -0700

    ex01.js
    Capitalize Letters

--- ex01.js
+++ ex01.js
@@ -3,3 +3,21 @@
 // Example: capitalizeWords("hello world") should return "Hello World".
 
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
+
+SOLUTION
+
+//Variable that stores the words
+const words = "hello world";
+
+function capitalizeWords(){
+  
+  //split array
+  const arrWords = words.split(" ");
+  
+  for ( let i = 0; i < arrWords.length; i++){
+    arrWords[i] = arrWords[i].slice(0,1).toUpperCase() + arrWords[i].slice(1);
+  }
+  
+  console.log(arrWords.join(" "));
+}
+  capitalizeWords();
