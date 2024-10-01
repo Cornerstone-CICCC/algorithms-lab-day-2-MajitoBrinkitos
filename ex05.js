7743cd94d32b5cd8f64e1088f12cb221e958a795
María-José Mejía <165747579+MajitoBrinkitos@users.noreply.github.com>
2024-10-01 15:38:00 -0700

    ex05.js
    count consonants

--- ex05.js
+++ ex05.js
@@ -2,4 +2,21 @@
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
 
-console.log(countConsonants("hello world")); // Expected output: 7
+//console.log(countConsonants("hello world")); // Expected output: 7
+
+const word = "hello world";
+
+function countConsonants(word){
+
+    let countConsonants = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for(i = 0; i < word.length; i++){
+        const letters = word.charAt(i);
+        if(!vowels.includes(letters) && /[a-z]/.test(letters)){
+            countConsonants++;
+        }
+    }
+    console.log(`${word} has ${countConsonants} consonants`);
+}
+    countConsonants(word);
