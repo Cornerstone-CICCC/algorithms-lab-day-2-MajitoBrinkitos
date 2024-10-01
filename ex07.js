a057236f28ca15fcff337068fbe13a2bdeb7894d
María-José Mejía <165747579+MajitoBrinkitos@users.noreply.github.com>
2024-10-01 15:12:40 -0700

    ex07.js
    valid email

--- ex07.js
+++ ex07.js
@@ -3,5 +3,26 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
-console.log(validateEmail("test@example.com")); // Expected output: true
-console.log(validateEmail("invalid-email.com")); // Expected output: false
+//console.log(validateEmail("test@example.com")); // Expected output: true
+//console.log(validateEmail("invalid-email.com")); // Expected output: false
+
+const email = "test@example.com";
+
+function validateEmail(email){
+
+    let thereIsSymbol = false;
+
+    for(let i = 0; i < email.length; i++){
+        if((email.charAt(i)) === '@'){
+            thereIsSymbol = true;
+            break;
+        }
+        }
+        if(thereIsSymbol){
+            console.log(`${email} is a valid email`);
+        } else {
+            console.log(`Try another email`);
+        }
+    }
+
+validateEmail(email);
