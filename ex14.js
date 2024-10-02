e6e5d4cc0c80fb3dd600b9804bbec2f85901cb20
Majito Brinkitos <mcvef2013@gmail.com>
2024-10-02 12:30:17 -0700

    shopping cart

--- ex14.js
+++ ex14.js
@@ -2,7 +2,31 @@
 // `addItem(item)` to add an item to the cart, and `getItems()` to retrieve the list of added items.
 // Example: const cart = createCart(); cart.addItem("apple"); cart.getItems(); // ["apple"]
 
+
+
+function createCart(){
+    let items =[]; //Here I will store my items
+
+    return {
+        //method addItem
+        addItem: function(item) {
+            items.push(item);
+        },
+
+        //method getItem
+        getItems: function() {
+            return items;
+        },
+
+        //method showCart
+        showCart: function(){
+            console.log(`The items in my cart: ${items}`);
+        }
+    };
+}
+
 const cart = createCart();
 cart.addItem("apple");
 cart.addItem("banana");
 console.log(cart.getItems()); // Expected output: ["apple", "banana"]
+cart.showCart();
